8a39aee3a91a5806782a20cc866eef09a1212761
Andrea Filice <help.playepik@gmail.com>
2024-12-21 19:50:56 +0100

    First pull

---
+++ js/home.js
@@ -0,0 +1,14 @@
+function OnLoad(){
+    document.getElementById("loaderLayer").style.animation = "FadeOut 1s linear";
+    document.getElementById("mainLayer").style.animation = "FadeIn 1s linear";
+    setTimeout(SetActiveLayer, 1000);
+}
+
+function SetActiveLayer(){
+    document.getElementById("loaderLayer").style.display = "none";
+    document.getElementById("mainLayer").style.display = "block";
+}
+
+function OpenGames(){
+    window.location.href = "games.html";
+}
---
+++ js/openPages.js
@@ -0,0 +1,19 @@
+function OpenCourseOfDeath(){
+    window.location.href = "courseOfDeath.html";
+}
+
+function OpenSquareRoyale(){
+    window.location.href = "squareRoyale.html";
+}
+
+function OpenRunners(){
+    window.location.href = "runners.html";
+}
+
+function OpenCubeInvasion(){
+    window.location.href = "cubeInvasion.html";
+}
+
+function OpenCodeUnlocker(){
+    window.location.href = "codeUnlocker.html";
+}
