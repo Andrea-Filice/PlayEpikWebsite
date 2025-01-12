9af0a5793f64b80e276a3f5415acc6ed41100b4c
Andrea Filice <help.playepik@gmail.com>
2025-01-12 17:05:11 +0100

    Fixing buttons on index.html

--- js/home.js
+++ js/home.js
@@ -1,3 +1,5 @@
+var scrollToReleaseNotes = false;
+
 function OnLoad(){
     document.getElementById("loaderLayer").style.animation = "FadeOut 1s linear";
     document.getElementById("mainLayer").style.animation = "FadeIn 1s linear";
@@ -5,8 +7,13 @@ function OnLoad(){
 }
 
 function SetActiveLayer(){
+    const scrollToReleaseNotes = sessionStorage.getItem("scrollToReleaseNotes") === "true";
     document.getElementById("loaderLayer").style.display = "none";
     document.getElementById("mainLayer").style.display = "block";
+    if (scrollToReleaseNotes) {
+        document.getElementById("release").scrollIntoView({ behavior: "smooth" });
+        sessionStorage.removeItem("scrollToReleaseNotes")
+    }
 }
 
 function OpenGames(){
@@ -19,4 +26,17 @@ function BackToTop(){
 
 function OpenServerStatus(){
     window.location.href = "serverStatus.html";
+}
+
+//OPEN RELEASE NOTES
+
+
+function OpenCourseOfDeathRN(){
+    sessionStorage.setItem("scrollToReleaseNotes", "true");
+    window.location.href = "courseOfDeath.html";
+}
+
+function OpenSquareRoyaleRN(){
+    sessionStorage.setItem("scrollToReleaseNotes", "true");
+    window.location.href = "squareRoyale.html";
 }
