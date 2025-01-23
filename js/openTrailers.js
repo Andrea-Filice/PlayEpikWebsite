dcd6aebe37dccaaa0f9470ad1db25394b4836b15
Andrea Filice <help.playepik@gmail.com>
2025-01-23 14:59:17 +0100

    Add tutorials on Course Of Death and Square Royale page!

---
+++ js/openTrailers.js
@@ -0,0 +1,7 @@
+function OpenCourseOfDeathTrailer(){
+    window.location.href = "https://www.youtube.com/watch?v=36jbWbFPBXI&ab_channel=PlayEpik%2CIncorporation.";
+}
+
+function OpenSquareRoyaleTrailer(){
+    window.location.href = "https://www.youtube.com/watch?v=trP4BQUTvP4&ab_channel=PlayEpik%2CIncorporation.";
+}
