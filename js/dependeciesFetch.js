4494a166385d4915b2780ab42ec5bcaacdcdb615
Andrea Filice <help.playepik@gmail.com>
2025-04-01 19:09:55 +0200

    Performance Improvements

--- js/dependeciesFetch.js
+++ js/dependeciesFetch.js
@@ -1,7 +1,11 @@
-fetch('dependecies/dependecies.json')
-.then(response => response.json())
-      .then(data => {
-        const version = data.versionUnity;
-        document.getElementById('unity').innerText = "Unity Engine v6: (" + version + ")";
-      })
-      .catch(error => console.error('Network error', error));
+try{
+  fetch('dependecies/dependecies.json')
+  .then(response => response.json())
+        .then(data => {
+          const version = data.versionUnity;
+          document.getElementById('unity').innerText = "Unity Engine v6: (" + version + ")";
+        })
+}
+catch (error) {
+  document.getElementById('unity').innerText = "Unity Engine v6: (" + error + ")";
+}
