58efa3f0b963065e5b3cc1ce427c0969edc380f9
Andrea Filice <help.playepik@gmail.com>
2025-04-21 19:19:51 +0200

    Fixing "SendEmail" bug

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function SendEmail(){
+    emailjs.init("Vx1Y2a2hCfrNX3MLW");
+    const email = document.getElementById('email').value;
+    const description = document.getElementById('description').value;
+    const templateParams = {
+      name: document.getElementById('email').value,
+      message: description,
+    };
+    emailjs.send('service_gf3scx2', 'template_idq9ig8', templateParams)
+    .then(function(response) {
+      console.log('SUCCESS!', response.status, response.text);
+      document.getElementById('responseMessage').textContent = "Feedback send successfully!";
+      document.getElementById('feedbackForm').reset();
+    }, function(error) {
+      console.error('Error sending the support mail: ', error);
+      document.getElementById('responseMessage').textContent = "There was an error sending your feedback. Please try again later.";
+    });
+}
