f3741e393ef534cdd121dce7a1a3e3850430fbb7
Play Epik Incorporation <help.playepik@gmail.com>
2025-09-26 19:45:19 +0200

    FIX: Fixing unity version
    * fixed unity version with the latest version

--- js/dependenciesFetch.js
+++ js/dependenciesFetch.js
@@ -2,7 +2,7 @@ try{
   fetch('https://playepikservercontents.netlify.app/dependencies/dependencies.json')
   .then(response => response.json())
         .then(data => {
-          document.getElementById('unity').innerText = "Unity Engine v6.1: (" + data.versionUnity + ")";
+          document.getElementById('unity').innerText = "Unity Engine v6.2: (" + data.versionUnity + ")";
         })
 }
-catch (error) {document.getElementById('unity').innerText = "Unity Engine v6.1: (Error retrieving data: " + error + ").";}
+catch (error) {document.getElementById('unity').innerText = "Unity Engine v6.2: (Error retrieving data: " + error + ").";}
