0c54b075b1196b4849e4a17f5370f4a2f101ef9b
Andrea-Filice <filiceandrea8@gmail.com>
2025-11-04 20:24:44 +0100

    fix: fixing small bugs

--- js/objectives.js
+++ js/objectives.js
@@ -4,9 +4,9 @@ document.addEventListener("scroll", () => {
   const animations = [
     { id: "counter", position: 400, animation: "counter 3s forwards" },
     { id: "counter2", position: 500, animation: "counter2 3s forwards" },
-    { id: "counter3", position: 1000, animation: "counter3 3s forwards" },
-    { id: "counter5", position: 1200, animation: "counter5 3s forwards" },
-    { id: "counter4", position: 2000, animation: "counter4 3s forwards" },
+    { id: "counter3", position: 800, animation: "counter3 3s forwards" },
+    { id: "counter5", position: 1000, animation: "counter5 3s forwards" },
+    { id: "counter4", position: 2350, animation: "counter4 3s forwards" },
   ];
 
   animations.forEach(({ id, position, animation }) => {
